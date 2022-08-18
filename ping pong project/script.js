d21ca9b61c22af3d047fc936a8ec359ba467dbd7
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-18 13:34:18 +0530

    Commit

--- ping pong project/script.js
+++ ping pong project/script.js
@@ -4,4 +4,7 @@ const Rod2 = document.getElementById("rod2");
 Rod1.style.left = Rod1.offsetLeft + "px";
 Rod2.style.left = Rod2.offsetLeft + "px";
 
-
+let rod1Height = Rod1.offsetHeight;
+let rod1Width = Rod1.offsetWidth;
+let rod2Height = Rod2.offsetHeight;
+let rod2Width = Rod2.offsetWidth;
