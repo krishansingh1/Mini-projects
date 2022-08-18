0f070424919680e28120c508c70bceab5850bc3b
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-18 13:32:10 +0530

    Commit

--- ping pong project/script.js
+++ ping pong project/script.js
@@ -1,6 +1,7 @@
 const Rod1 = document.getElementById("rod1");
 const Rod2 = document.getElementById("rod2");
 
-Rod1.style.left = Rod1.offsetLeft;
-Rod2.style.left = Rod2.offsetLeft;
+Rod1.style.left = Rod1.offsetLeft + "px";
+Rod2.style.left = Rod2.offsetLeft + "px";
+
 
