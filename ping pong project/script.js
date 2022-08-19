5bc20cf4c5352802552ac0c25d98f1e01b715cb9
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-19 14:43:10 +0530

    Commit

--- ping pong project/script.js
+++ ping pong project/script.js
@@ -3,6 +3,8 @@ let initial_ball = document.getElementById("ball");
 let ball = document.getElementById("ball");
 let Rod1 = document.getElementById("rod1");
 let Rod2 = document.getElementById("rod2");
+let Rod1_coord = Rod1.getBoundingClientRect();
+let Rod2_coord = Rod2.getBoundingClientRect();
 let initial_ball_cord = ball.getBoundingClientRect();
 let ball_cord = initial_ball_cord;
 let board = document.getElementById("board");
@@ -64,5 +66,33 @@ function moveHorizontally(element) {
     if (left2 < window.innerWidth - rod2Width - 15) {
       Rod2.style.left = setValue(left2 + 20);
     }
+  }
+}
+
+function moveBall(dx, dy, dxd, dyd) {
+  if (ball_cord.top <= board_cord.top) {
+    dyd = 1;
+  }
+  if (ball_cord.bottom >= board_cord.bottom) {
+    dyd = 0;
+  }
+  if (
+    ball_cord.left <= Rod1_coord.right &&
+    ball_cord.top >= Rod1_coord.top &&
+    ball_cord.bottom <= Rod1_coord.bottom
+  ) {
+    dxd = 1;
+    dx = Math.floor(Math.random() * 4) + 3;
+    dy = Math.floor(Math.random() * 4) + 3;
+  }
+
+  if (
+    ball_cord.right >= Rod2_coord.left &&
+    ball_cord.top >= Rod2_coord.top &&
+    ball_cord.bottom <= Rod2_coord.bottom
+  ) {
+    dxd = 0;
+    dx = Math.floor(Math.random() * 4) + 3;
+    dy = Math.floor(Math.random() * 4) + 3;
   }
 }
