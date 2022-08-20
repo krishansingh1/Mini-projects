f7ce8d8494504f6c4ce234fd26b1f3289cd604ef
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-20 12:44:00 +0530

    Commit

--- ping pong project/script.js
+++ ping pong project/script.js
@@ -1,26 +1,22 @@
 let gameState = "start";
-let initial_ball = document.getElementById("ball");
-let ball = document.getElementById("ball");
 let Rod1 = document.getElementById("rod1");
 let Rod2 = document.getElementById("rod2");
+let board = document.getElementById("board");
+let initial_ball = document.getElementById("ball");
+let ball = document.getElementById("ball");
+let score_1 = document.getElementById("player_1_score");
+let score_2 = document.getElementById("player_2_score");
 let Rod1_coord = Rod1.getBoundingClientRect();
 let Rod2_coord = Rod2.getBoundingClientRect();
 let initial_ball_cord = ball.getBoundingClientRect();
 let ball_cord = initial_ball_cord;
-let board = document.getElementById("board");
 let board_cord = board.getBoundingClientRect();
+let Rod_common = document.querySelector(".rod").getBoundingClientRect();
 
 let dx = Math.floor(Math.random() * 4) + 3;
 let dy = Math.floor(Math.random() * 4) + 3;
 let dxd = Math.floor(Math.random() * 2);
 let dyd = Math.floor(Math.random() * 2);
-
-let score1 = 0;
-let score2 = 0;
-localStorage.setItem("Score1", score1);
-localStorage.setItem("Score2", score2);
-const latestScore1 = localStorage.getItem("Score1");
-const latestScore2 = localStorage.getItem("Score2");
 
 document.addEventListener("keydown", (e) => {
   if ((e.key = "Enter")) {
@@ -37,49 +33,20 @@ document.addEventListener("keydown", (e) => {
   }
 
   if (gameState == "play") {
-    Rod1.style.left = Rod1.offsetLeft + "px";
-    Rod2.style.left = Rod2.offsetLeft + "px";
+    if (e.key == "w" || e.key == "W") {
+      Rod1.style.top =
+        Math.max(board_cord.top, Rod1_coord.top - window.innerHeight * 0.06) +
+        "px";
+      Rod1_coord = Rod1.getBoundingClientRect();
 
-    let rod1Width = Rod1.offsetWidth;
-    let rod2Width = Rod2.offsetWidth;
-
-    window.addEventListener("keypress", (e) => {
-      e.preventDefault();
-      moveHorizontally(e);
-    });
-
-    function setValue(value) {
-      return value + "px";
+      Rod2.style.top =
+        Math.max(board_cord.top, Rod2_coord.top - window.innerHeight * 0.06) +
+        "px";
+      Rod2_coord = Rod2.getBoundingClientRect();
     }
 
-    function moveHorizontally(element) {
-      let left1 = parseInt(Rod1.style.left);
-      let left2 = parseInt(Rod2.style.left);
-
-      if (
-        element.key === "a" ||
-        element.key === "A" ||
-        element.keyCode === "37"
-      ) {
-        if (left1 > 15) {
-          Rod1.style.left = setValue(left1 - 10);
-        }
-        if (left2 > 15) {
-          Rod2.style.left = setValue(left2 - 10);
-        }
-      }
-      if (
-        element.key === "d" ||
-        element.key === "D" ||
-        element.keyCode === "39"
-      ) {
-        if (left1 < window.innerWidth - rod1Width - 15) {
-          Rod1.style.left = setValue(left1 + 10);
-        }
-        if (left2 < window.innerWidth - rod2Width - 15) {
-          Rod2.style.left = setValue(left2 + 10);
-        }
-      }
+    if (e.key == "s" || e.key == "S") {
+      Rod1.style.top=Math.min(board_cord.bottom -Rod)
     }
   }
 });
