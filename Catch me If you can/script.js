a2f6ac7217890766b5d11da84b934c718d5e2d03
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-25 13:24:15 +0530

    Commit

--- Catch me If you can/script.js
+++ Catch me If you can/script.js
@@ -9,9 +9,17 @@
 // console.log(promise);
 // promise.then((data) => console.log(data));
 // promise.catch((data) => console.log(data));
-
+console.log("Your order is being placed place wait until your order is ready");
 async function a() {
-  return Promise.reject("Something went wrong");
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // resolve("Your order is ready");
+      reject("Something went wrong");
+    }, 2000);
+  });
+  let result = await promise;
+  return result;
 }
 console.log(a());
+// a().then((data) => console.log(data));
 a().catch((data) => console.log(data));
