f56ebae60948c8a0df449c9e5fe5044dc9acc80f
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-25 19:56:53 +0530

    Commit

--- Catch me If you can/script.js
+++ Catch me If you can/script.js
@@ -9,17 +9,37 @@
 // console.log(promise);
 // promise.then((data) => console.log(data));
 // promise.catch((data) => console.log(data));
-console.log("Your order is being placed place wait until your order is ready");
-async function a() {
-  let promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // resolve("Your order is ready");
-      reject("Something went wrong");
-    }, 2000);
+// console.log("Your order is being placed place wait until your order is ready");
+// async function a() {
+//   let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       // resolve("Your order is ready");
+//       reject("Something went wrong");
+//     }, 2000);
+//   });
+//   let result = await promise;
+//   return result;
+// }
+// console.log(a());
+// // a().then((data) => console.log(data));
+// a().catch((data) => console.log(data));
+
+const promise1 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("a");
   });
-  let result = await promise;
-  return result;
-}
-console.log(a());
-// a().then((data) => console.log(data));
-a().catch((data) => console.log(data));
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  resolve("b");
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("c");
+  }, 10);
+});
+
+Promise.race([promise1, promise2, promise3]).then((msg) => {
+  console.log(msg);
+});
