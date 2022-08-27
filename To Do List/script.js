349ca01160a3213c7427ce5d54321d9527b086a9
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-27 23:47:23 +0530

    Commit

---
+++ To Do List/script.js
@@ -0,0 +1,21 @@
+//Tasks
+const tasks = [];
+
+const taskList = document.getElementById("list");
+const addTaskInput = document.getElementById("add");
+const tasksCounter = document.getElementById("tasks-counter");
+console.log("working");
+//Function for appending the list
+function renderList() {}
+
+//Function for add tasks
+function addTask(task) {}
+
+//Function for deleting tasks
+function deleteTask(taskId) {}
+
+//Function for Check Todo/Task
+function markTaskAsComplete(taskId) {}
+
+//Function for showNotifications
+function showNotification(text) {}
