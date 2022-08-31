c982d605732014c39aa6799b1d7888259090a32f
Krishan Kumar <kkrishan7074@gmail.com>
2022-08-31 11:36:26 +0530

    Commit

--- To Do List/script.js
+++ To Do List/script.js
@@ -6,17 +6,20 @@ const tasksCounter = document.getElementById("tasks-counter");
 
 //Function to fetch data from an API
 
-function fetchData() {
-  fetch("https://jsonplaceholder.typicode.com/todos")
-    .then((Response) => Response.json())
-    .then((data) => {
-      tasks = data.slice(0, 10);
-      console.log(tasks);
-      renderList();
-    })
-    .catch((error) => {
-      console.log("Error:", error);
-    });
+async function fetchData() {
+  // fetch("https://jsonplaceholder.typicode.com/todos")
+  //   .then((Response) => Response.json())
+  //   .then((data) => {
+  //     tasks = data.slice(0, 10);
+  //     console.log(tasks);
+  //     renderList();
+  //   })
+  //   .catch((error) => {
+  //     console.log("Error:", error);
+  //   });
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  console.log(response);
+  return await response.json();
 }
 
 //Function to Add Task to Dom
