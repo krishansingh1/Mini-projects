ba8d05c06138639c45bb149d4b58e5490404a364
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-01 17:34:21 +0530

    Commit

--- Calculator project/script.js
+++ Calculator project/script.js
@@ -5,19 +5,28 @@ let operand1 = 0;
 let operand2 = null;
 let operator = null;
 
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener("click", () => {
-    let value = button[i].dataset.value;
 
-    if (value == "+") {
-      operator = "+";
-      operand1 = parseFloat(display.textContent);
-    } else if (value == "=") {
-      operand2 = parseFloat(display.textContent);
-      let result = "operand1 operator operand2";
-      console.log(eval(result));
-    } else {
-      display.innerText += value;
-    }
-  });
-}
+
+
+
+
+
+
+
+
+// for (let i = 0; i < button.length; i++) {
+//   button[i].addEventListener("click", () => {
+//     let value = button[i].dataset.value;
+
+//     if (value == "+") {
+//       operator = "+";
+//       operand1 = parseFloat(display.textContent);
+//     } else if (value == "=") {
+//       operand2 = parseFloat(display.textContent);
+//       let result = "operand1 operator operand2";
+//       console.log(eval(result));
+//     } else {
+//       display.innerText += value;
+//     }
+//   });
+// }
