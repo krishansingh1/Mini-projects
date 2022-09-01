6a8ef445035f90cfd86fd3425cf43ea8ab7218b0
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-02 01:20:29 +0530

    Commit

--- Calculator project/script.js
+++ Calculator project/script.js
@@ -1,11 +1,18 @@
 const buttons = document.querySelectorAll(".button");
-
+const result = document.querySelector(".input");
 let string = "";
-
-console.log(buttons);
 
 Array.from(buttons).forEach((button) => {
   button.addEventListener("click", (e) => {
-    console.log(e.target.innerHTML);
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      result.value = string;
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+      result.value = string;
+    } else {
+      string = string + e.target.innerHTML;
+      result.value = string;
+    }
   });
 });
