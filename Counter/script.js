72ba801ab64bc13a268ac723bfa2aef319bfc674
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-04 12:45:35 +0530

    Commit

--- Counter/script.js
+++ Counter/script.js
@@ -52,7 +52,7 @@ function startCounter() {
     return;
   }
 
-  let currentNo = document.querySelector(".counter .current");
+  let currentNo = document.querySelector(".counter .current_one");
   let nextNo = document.querySelector(".counter .next");
   let count = 0;
 
