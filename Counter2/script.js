f22743c04524c6a354deea87d1214b0ee33bb699
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-09 17:32:38 +0530

    Commit

--- Counter2/script.js
+++ Counter2/script.js
@@ -1,7 +1,11 @@
-const input = document.getElementById("num_counter_input");
-
 const button = document.getElementById("btn_counter");
 
 button.addEventListener("click", () => {
-    
+  const number = parseInt(document.getElementById("num_counter_input"));
+  console.log(number);
+
+  if (isNaN(number)) {
+    window.alert("Please Enter a Number!");
+    return;
+  }
 });
