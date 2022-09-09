b047a5a9a62db59a7f35aff562417823592e35ba
Krishan Kumar <kkrishan7074@gmail.com>
2022-09-09 17:48:52 +0530

    Commit

--- Counter2/script.js
+++ Counter2/script.js
@@ -1,11 +1,16 @@
 const button = document.getElementById("btn_counter");
+const input = document.getElementById("num_counter_input");
 
 button.addEventListener("click", () => {
-  const number = parseInt(document.getElementById("num_counter_input"));
+  const number = parseInt(input.value);
   console.log(number);
 
   if (isNaN(number)) {
     window.alert("Please Enter a Number!");
     return;
+  }
+
+  if (number < 1 || number > 99999) {
+    window.alert("Range of Bound! Please Enter a number between 1 and 99999");
   }
 });
